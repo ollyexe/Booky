e2647fabd4b3cd8c0b9416482aa090a97fdd3602
Oliviu Gratii <oliviu.gratii@edu.unito.it>
2022-11-22 15:02:42 +0100

    WIP ontap professor card

---
+++ lib/MainPages/professorSelected/selectedProfessor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class selectedProfessor extends StatefulWidget {
+  const selectedProfessor({Key? key}) : super(key: key);
+
+  @override
+  State<selectedProfessor> createState() => _selectedProfessor();
+}
+
+class _selectedProfessor extends State<selectedProfessor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
